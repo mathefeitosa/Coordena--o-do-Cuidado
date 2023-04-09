3bb547fc7985a023a98b678c78d5611a66f68791
Feitosa <mathe.feitosa@gmail.com>
2023-04-09 17:45:15 -0300

    added user_types field to profile

--- users/models.py
+++ users/models.py
@@ -2,35 +2,9 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-STATE_CHOICES = (
-  ("AC", "Acre"),
-  ("AL", "Alagoas"),
-  ("AP", "Amapá"),
-  ("AM", "Amazonas"),
-  ("BA", "Bahia"),
-  ("CE", "Ceará"),
-  ("DF", "Distrito Federal"),
-  ("ES", "Espírito Santo"),
-  ("GO", "Goiás"),
-  ("MA", "Maranhão"),
-  ("MT", "Mato Grosso"),
-  ("MS", "Mato Grosso do Sul"),
-  ("MG", "Minas Gerais"),
-  ("PA", "Pará"),
-  ("PB", "Paraíba"),
-  ("PR", "Paraná"),
-  ("PE", "Pernambuco"),
-  ("PI", "Piauí"),
-  ("RJ", "Rio de Janeiro"),
-  ("RN", "Rio Grande do Norte"),
-  ("RS", "Rio Grande do Sul"),
-  ("RO", "Rondônia"),
-  ("RR", "Roraima"),
-  ("SC", "Santa Catarina"),
-  ("SP", "São Paulo"),
-  ("SE", "Sergipe"),
-  ("TO", "Tocantins"),
-)
+from ..utils.constants import STATE_CHOICES, USER_TYPES
+
+
 
 class Profile(models.Model):
   id = models.UUIDField(
@@ -44,6 +18,14 @@ class Profile(models.Model):
       null=True,
       blank=True,
       verbose_name='Usuário',
+  )
+  user_type = models.CharField(
+      max_length=3,
+      choices=USER_TYPES,
+      verbose_name='Tipo de usuário',
+      blank=False,
+      null=False,
+      editable=True
   )
   full_name = models.CharField(
       max_length=200,
